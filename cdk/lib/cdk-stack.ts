13512b71271bfebc1f6bab9f0f9b187061904e72
Steven Tsang <tsangste@gmail.com>
2021-07-20 22:39:15 +0100

    deployment yay!

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core'
 import { Vpc } from '@aws-cdk/aws-ec2'
 import { Cluster, ContainerImage } from '@aws-cdk/aws-ecs'
+import { Repository } from '@aws-cdk/aws-ecr'
 import { ApplicationLoadBalancedFargateService } from '@aws-cdk/aws-ecs-patterns'
 
 export class CdkStack extends cdk.Stack {
@@ -10,11 +11,14 @@ export class CdkStack extends cdk.Stack {
     const vpc = new Vpc(this,'MyVpc',{ maxAzs: 2 })
     const cluster = new Cluster(this ,'Cluster',{ vpc })
 
+    const repo = new Repository(this, 'healthyfish')
+
     const myService = new ApplicationLoadBalancedFargateService(this, 'HealthyFish', {
       cluster,
-      desiredCount:3,
+      desiredCount:1,
       taskImageOptions: {
-        image: ContainerImage.fromAsset('apps/myapp')
+        image: ContainerImage.fromEcrRepository(repo, '1.0.1'),
+        containerPort: 8080
       }
     })
 
--- main.go
+++ main.go
@@ -6,9 +6,8 @@ import (
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	"github.com/tsangste/HealthyFish/controllers"
+	"github.com/tsangste/HealthyFish/docs"
 	"net/http"
-
-	_ "github.com/tsangste/HealthyFish/docs"
 )
 
 // @title HealthyFish API
@@ -27,7 +26,10 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
 	})
 
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.GET("/swagger/*any", func(context *gin.Context) {
+		docs.SwaggerInfo.Host = context.Request.Host
+		ginSwagger.CustomWrapHandler(&ginSwagger.Config{URL: "/swagger/doc.json"}, swaggerFiles.Handler)(context)
+	})
 
 	r.GET("/items", controllers.GetAll)
 	r.GET("/items/calculate/:amount", controllers.CalculateItems)
